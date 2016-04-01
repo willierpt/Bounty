d615a55c5f53aecd64fc69e52f4b9164026c5a39
willierpt <willier.ca@gmail.com>
2016-04-01 09:21:49 +0100

    Change-Id: I00dccb2edd5926f0f11fd6be2e60569a5112f5ff Signed-off-by: willierpt <willier.ca@gmail.com>

--- Projecto Final/src/Bounty/FusosHorarios.java
+++ Projecto Final/src/Bounty/FusosHorarios.java
@@ -25,6 +25,7 @@ public class FusosHorarios implements Serializable {
 		fusosHorarios.add(new FusosHorarios("Madrid",1));
 		fusosHorarios.add(new FusosHorarios("Sydney",10));
 		fusosHorarios.add(new FusosHorarios("Paris",1));
+		fusosHorarios.add(new FusosHorarios("Lima", -5));
 
 		
 		
