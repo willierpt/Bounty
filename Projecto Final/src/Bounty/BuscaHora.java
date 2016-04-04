83858a4f90cf1691369fcdb8e1e298c1a0f800b1
willierpt <willier.ca@gmail.com>
2016-04-04 11:48:23 +0100

    Change-Id: I0eed3b7d56cd05e64d92e2e6256b603c4fc0116f Signed-off-by: willierpt <willier.ca@gmail.com>

--- Projecto Final/src/Bounty/BuscaHora.java
+++ Projecto Final/src/Bounty/BuscaHora.java
@@ -10,13 +10,7 @@ public class BuscaHora {
 	static BuscaHora tempo = new BuscaHora();
 
 	public static void main(String[] args) {
-		//for comit
-		ArrayList<FusosHorarios> fusosHorarios = new ArrayList<FusosHorarios>();
-		fusosHorarios.add(new FusosHorarios("Lisboa",0));
-		fusosHorarios.add(new FusosHorarios("Tokio",10));
-		fusosHorarios.add(new FusosHorarios("New York",-5));
-		fusosHorarios.add(new FusosHorarios("Açores",-1));
-		tempo.getRelogioFuso("Tokio",fusosHorarios);
+		
 	}
 
 	public String getRelogio(){
